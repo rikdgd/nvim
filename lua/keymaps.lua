6d6e8fb449393765977596275acb44e845b37b94
Rik de Goede <rik.dgd@gmail.com>
2025-05-15 00:33:50 +0200

    Added a keybinding to navigate to the file tree

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,7 +7,10 @@ local opts = { noremap = true, silent = true }
 keymap("n", "<leader>w", ":w<CR>", opts)                 -- Save
 keymap("n", "<leader>q", ":q<CR>", opts)                 -- Quit
 keymap("n", "<leader>h", ":nohlsearch<CR>", opts)        -- Clear search highlight
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)    -- Toggle file tree
+
+-- File tree
+keymap("n", "<leader>et", ":NvimTreeToggle<CR>", opts)    -- Toggle file tree
+keymap("n", "<leader>e", ":NvimTreeFocus<CR>", opts)      -- Navigate to file tree
 
 -- Telescope
 keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
